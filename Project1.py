b9341f1efebcb9770dc53c1ee4d01ba8c2f23b48
wimsparenberg <wsparenberg@torontomu.ca>
2025-10-02 11:28:16 -0400

    Data visualization continued

--- Project1.py
+++ Project1.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 
+#1 - Data Processing
 df = pd.read_csv("Data/Project 1 Data.csv")
 
-
+#2 - Data Visualization
 desc_stat = df[['X', 'Y', 'Z',]].describe()
 desc_stat_step = df['Step'].value_counts().sort_index()
-plt(df.hist())
+df.hist()
 
